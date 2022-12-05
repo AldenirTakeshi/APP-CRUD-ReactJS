b5a69442b99e4a1f3058183d1a31a6fcf8d36f71
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-05 09:41:38 -0400

    add logo

--- FrontEnd/src/components/template/Logo.jsx
+++ FrontEnd/src/components/template/Logo.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "../../assets/imgs/Starbucks-logo.png";
+import "./Logo.css";
 
 const logo = () => {
-  return <aside>Logo</aside>;
+  return (
+    <aside className="logo">
+      <a href="/" className="logo">
+        <img src={Logo} style={{ maxWidth: "120px" }} alt="StarBucks-logo" />
+      </a>
+    </aside>
+  );
 };
 
 export default logo;
