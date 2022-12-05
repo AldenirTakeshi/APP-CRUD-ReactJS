4f62a0e130a93d79d3faaca1b4b915e27bb02ce3
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-05 17:29:04 -0400

    removido npm na pasta principal/ add footer

--- FrontEnd/src/components/template/Footer.jsx
+++ FrontEnd/src/components/template/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import "./Footer.css";
+import { FaHeart } from "react-icons/fa";
 const Footer = () => {
-  return <footer className="footer">Rodapé</footer>;
+  return (
+    <footer className="footer">
+      <span>
+        Desenvolvido com{" "}
+        <i>
+          <FaHeart style={{ color: "red" }} />
+        </i>{" "}
+        por Cod
+        <span style={{ color: "red" }}>3</span>r
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
